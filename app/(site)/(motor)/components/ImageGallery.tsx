6e404e11347cfc9c4bc2a251754956f51a814113
pavelhaj93 <pavel.hajduch@actumdigital.com>
2023-08-20 19:38:03 +0200

    fix image gallery

--- app/(site)/(motor)/components/ImageGallery.tsx
+++ app/(site)/(motor)/components/ImageGallery.tsx
@@ -16,8 +16,12 @@ const ImageGallery: FC<ImageGalleryProps> = ({ motor }) => {
         <Image
           src={motor.images?.[index] ?? "/images/placeholder.png"}
           alt={motor.name}
-          width={400}
-          height={650}
+          width={384}
+          height={480}
+          style={{
+            maxHeight: "480px",
+            width: "auto",
+          }}
         />
       </div>
       <div className="flex flex-row gap-5 mt-5">
