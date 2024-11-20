003ca302d4be8ab8d092bd0f3220cde77f0a97c6
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-20 21:32:59 +0100

    feat:  try different error handling

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -11,25 +11,33 @@ export async function POST(req: Request) {
     }
 
     try {
-      const info = await transporter.sendMail({
-        ...mailOptions,
-        subject: `Nová zpráva od - email ${email}`,
-        text: message,
-        html: `<h2>Nová zpráva od - email ${email}</h2><br></br><p>${message}</p><br></br>>`,
-      });
-
-      console.log("Email sent:", info);
-      return NextResponse.json(
-        { message: "Message sent successfully" },
-        { status: 200 }
+      const info = await transporter.sendMail(
+        {
+          ...mailOptions,
+          subject: `Nová zpráva od - email ${email}`,
+          text: message,
+          html: `<h2>Nová zpráva od - email ${email}</h2><br></br><p>${message}</p><br></br>>`,
+        },
+        (err, info) => {
+          if (err) {
+            console.error("Failed to send email:", err);
+            return NextResponse.json(err, { status: 500 });
+          }
+          if (info) {
+            console.log("Email sent:", info);
+            return NextResponse.json(info, { status: 200 });
+          }
+        }
       );
     } catch (err) {
-      console.error("Failed to send email:", err);
-      return NextResponse.json(
-        { error: "Failed to send the message" },
-        { status: 500 }
-      );
+      console.error(err);
+      return new NextResponse("Failed to send the message", { status: 500 });
     }
+
+    return NextResponse.json(
+      { message: "Message sent successfully" },
+      { status: 200 }
+    );
   } catch (err) {
     console.error(err);
     return new NextResponse("Internal server error", { status: 500 });
