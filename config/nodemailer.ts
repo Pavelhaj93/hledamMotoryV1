5e4c429e4034d4deec88c1371cbdcd465fc8523c
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-20 13:14:29 +0100

    fix: fix the not functioning contact form

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,17 +1,23 @@
 import nodemailer from "nodemailer";
-import { env } from "process";
 
-const email = env.EMAIL;
-const pass = env.EMAIL_PASSWORD;
+import dotenv from "dotenv";
+dotenv.config();
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASSWORD;
 
 export const transporter = nodemailer.createTransport({
-  host: "mailproxy.webglobe.com", // Replace with Webglobe.cz SMTP server address
+  host: "mail.webglobe.cz", // Replace with Webglobe.cz SMTP server address
   port: 587, // Use the appropriate port number
   secure: false, // Set to true if using a secure connection (like SSL/TLS)
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
+    user: email,
+    pass: pass,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  debug: true,
 });
 
 export const mailOptions = {
