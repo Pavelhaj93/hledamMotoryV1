ce941c7995176567c0c0a3869c5424a795c54e31
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-21 08:27:35 +0100

    feat: change proxy webglobe

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "mail.webglobe.cz", // Replace with Webglobe.cz SMTP server address
+  host: "mailproxy.webglobe.com", // Replace with Webglobe.cz SMTP server address
   port: 587, // Use the appropriate port number
   secure: false, // Set to true if using a secure connection (like SSL/TLS)
   auth: {
