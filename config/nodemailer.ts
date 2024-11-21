3e53280072289360a971bb2850ec1298e25be0a6
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-21 07:26:58 +0100

    feat: make nodemailer envs hardcoded

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,18 +1,12 @@
 import nodemailer from "nodemailer";
 
-import dotenv from "dotenv";
-dotenv.config();
-
-const email = process.env.EMAIL;
-const pass = process.env.EMAIL_PASSWORD;
-
 export const transporter = nodemailer.createTransport({
   host: "mail.webglobe.cz", // Replace with Webglobe.cz SMTP server address
   port: 587, // Use the appropriate port number
   secure: false, // Set to true if using a secure connection (like SSL/TLS)
   auth: {
-    user: email,
-    pass: pass,
+    user: "info@hledammotory.cz",
+    pass: "Neuro164,
   },
   tls: {
     rejectUnauthorized: false,
@@ -21,6 +15,6 @@ export const transporter = nodemailer.createTransport({
 });
 
 export const mailOptions = {
-  from: email,
-  to: email,
+  from: 'info@hledammotory.cz',
+  to: "info@hledammotory.cz",
 };
