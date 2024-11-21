e8299f024ef57fe5cf3012e65384855c9668f3eb
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-21 08:02:59 +0100

    feat: try promise version

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -10,19 +10,34 @@ export async function POST(req: Request) {
       return new NextResponse("Missing fields", { status: 400 });
     }
 
-    transporter.sendMail({
-      ...mailOptions,
-      subject: `Nová zpráva od - email ${email}`,
-      text: message,
-      html: `<h2>Nová zpráva od - email ${email}</h2><br></br><p>${message}</p><br></br>>`,
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          ...mailOptions,
+          subject: `Nová zpráva od - email ${email}`,
+          text: message,
+          html: `<h2>Nová zpráva od - email ${email}</h2><br></br><p>${message}</p><br></br>>`,
+          replyTo: email,
+        },
+        (err, info) => {
+          if (err) {
+            console.error("err", err);
+            reject(err);
+          }
+          if (info) {
+            console.log("info", info);
+            resolve(info);
+          }
+        }
+      );
     });
 
-    return NextResponse.json(
-      { message: "Message sent successfully" },
-      { status: 200 }
-    );
+    return NextResponse.json({ message: "Message sent successfully" });
   } catch (err) {
     console.error("Failed to send email:", err);
-    return new NextResponse("Internal server error", { status: 500 });
+    return NextResponse.json(
+      { error: err || "An error occurred" },
+      { status: 500 }
+    );
   }
 }
