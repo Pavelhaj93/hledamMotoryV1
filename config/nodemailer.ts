ab932181c2fa5d152f5fe17bf3563ff3f108839d
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-21 08:10:07 +0100

    feat: try to change process env

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,12 +1,15 @@
 import nodemailer from "nodemailer";
 
+const mail = process.env.MAIL;
+const password = process.env.EMAIL_PASSWORD;
+
 export const transporter = nodemailer.createTransport({
   host: "mail.webglobe.cz", // Replace with Webglobe.cz SMTP server address
   port: 587, // Use the appropriate port number
   secure: false, // Set to true if using a secure connection (like SSL/TLS)
   auth: {
-    user: "info@hledammotory.cz",
-    pass: "Neuro164",
+    user: mail,
+    pass: password,
   },
   tls: {
     rejectUnauthorized: false,
@@ -15,6 +18,6 @@ export const transporter = nodemailer.createTransport({
 });
 
 export const mailOptions = {
-  from: "info@hledammotory.cz",
-  to: "info@hledammotory.cz",
+  from: mail,
+  to: mail,
 };
